03b654a6723c314147d250c38e904c8cb6af4c1a
kostasp31 <kostaspetrakis2003@gmail.com>
2026-01-03 02:07:56 +0200

    transition to grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 import ChangeTheme from './ChangeTheme';
@@ -19,10 +19,10 @@ function App() {
 
   const colors = [
     { index: 1, mood: 'TERRIBLE', color: '#4B5563' },
-    { index: 2, mood: 'BAD',      color: '#9CA3AF' },
-    { index: 3, mood: 'NEUTRAL',  color: '#FDE68A' },
-    { index: 4, mood: 'GOOD',     color: '#86EFAC' },
-    { index: 5, mood: 'PERFECT',  color: '#93C5FD' }
+    { index: 2, mood: 'BAD', color: '#9CA3AF' },
+    { index: 3, mood: 'NEUTRAL', color: '#FDE68A' },
+    { index: 4, mood: 'GOOD', color: '#86EFAC' },
+    { index: 5, mood: 'PERFECT', color: '#93C5FD' }
   ];
 
   useEffect(() => {
@@ -36,10 +36,10 @@ function App() {
     }
 
     const _moods = months.map((month, index) => {
-      let lastdate = new Date(year, index+1, 0).getDate();
+      let lastdate = new Date(year, index + 1, 0).getDate();
 
       let moodsArayMonth = [];
-      for (let j=1; j<=lastdate; j++) {
+      for (let j = 1; j <= lastdate; j++) {
         moodsArayMonth.push({
           day: j,
           mood: null // Math.floor(Math.random() * (5 - 1 + 1)) + 1
@@ -48,7 +48,7 @@ function App() {
       return moodsArayMonth;
     });
 
-    localStorage.setItem('mood-calendar-data', JSON.stringify({[year]: _moods}));
+    localStorage.setItem('mood-calendar-data', JSON.stringify({ [year]: _moods }));
 
     setMoods(_moods);
   }, []);
@@ -69,11 +69,11 @@ function App() {
 
     const currentMood = newMoods[month][day];
     if (currentMood) {
-      let newMood = {...currentMood, mood: currentMood.mood + 1};
+      let newMood = { ...currentMood, mood: currentMood.mood + 1 };
       if (newMood.mood > 5) newMood.mood = 1;
 
       newMoods[month][day] = newMood;
-      localStorage.setItem('mood-calendar-data', JSON.stringify({[year]: newMoods}));
+      localStorage.setItem('mood-calendar-data', JSON.stringify({ [year]: newMoods }));
       setMoods(newMoods);
     }
   }
@@ -83,52 +83,79 @@ function App() {
   return (
     <div className='grandparent-container'>
       <Title />
+      <ChangeTheme theme={theme} setTheme={setTheme} />
       <div className='parent-container'>
-        <ChangeTheme theme={theme} setTheme={setTheme} />
-        <table style={{ borderCollapse: 'collapse' }}>
-          <thead>
-            <tr>
-              <td>
-              </td>
-              {months.map((month, index) => 
-                <th key={index}>
-                  {month.substring(0,2)}
-                </th>
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from(Array(31).keys()).map((day, index) => 
-              <tr key={index}>
-                <th className='numbers-th'>
-                  {day+1}
-                </th>
-                
-                {
-                  Array.from(Array(12).keys()).map((month, index) => 
-                    <td key={index} onClick={(e) => changeMood(e, month, day)} 
-                    style={{
-                      border: moods[month]?.[day]?.mood !== undefined ? `1px solid ${theme === 'dark' ? '#ececec' : '#242424'}` : 'none',
-                      background: moods[month]?.[day]?.mood ? colors[moods[month]?.[day]?.mood - 1]?.color : theme === 'light' ? '#ececec' : '#242424'
-                    }}>
-                      {/* {moods[month]?.[day]?.mood ?? null} */}
-                    </td>
-                  )
+        <div className='calendar' style={{backgroundColor: theme === 'dark' ? 'rgb(65, 65, 65)' : 'rgba(216, 216, 216, 1)'}}>
+          {Array.from({ length: 32 }).map((_, dayIndex) => (
+            dayIndex === 0 ?
+              <React.Fragment key={dayIndex}>
+                {Array.from({ length: 13 }).map((_, _monthIndex) => {
+                  return (
+                    <div className='calendar-item calendar-month' key={`${_monthIndex}-${dayIndex}`}
+                      style={{
+                        width: '40px',
+                        height: '40px'
+                      }}>
+                      {_monthIndex === 0 ? '' : months?.[_monthIndex - 1]?.substring(0, 2)}
+                    </div>
+                  );
+
                 }
+                )}
+              </React.Fragment>
+              :
+              <React.Fragment key={dayIndex}>
+                {Array.from({ length: 13 }).map((_, _monthIndex) => {
+                  const monthIndex = _monthIndex - 1;
 
-              </tr>
-            )}
-          </tbody>
-        </table>
+                  if (monthIndex === -1) {
+                    return (
+                      <div className='calendar-item calendar-day' key={`${monthIndex}-${dayIndex}`}
+                        style={{
+                          width: '40px',
+                          height: '40px'
+                        }}>
+                        {dayIndex}
+                      </div>
+                    )
+                  } else {
+                    if (moods[monthIndex]?.[dayIndex - 1]) {
+                      return (
+                        <div className='calendar-item calendar-mood-box' key={`${monthIndex}-${dayIndex - 1}`} onClick={(e) => changeMood(e, monthIndex, dayIndex - 1)}
+                          style={{
+                            width: '40px',
+                            height: '40px',
+                            animationDelay: `${Math.round((2000 / 31) * Math.round(Math.random() * dayIndex))}ms`,
+                            background: moods[monthIndex]?.[dayIndex - 1]?.mood ? colors[moods[monthIndex]?.[dayIndex - 1]?.mood - 1]?.color : theme === 'light' ? '#ececec' : '#242424'
+                          }}>
+                          {/* {moods[monthIndex]?.[dayIndex - 1]?.mood ?? null} */}
+                        </div>
+                      );
+                    }
+                    else
+                      return (
+                        <div className='calendar-item calendar-empty' key={`${monthIndex}-${dayIndex - 1}`}
+                          style={{
+                            width: '40px',
+                            height: '40px',
+                            background: 'transparent'
+                          }} />)
+                  }
+                }
+                )}
+              </React.Fragment>
+          ))}
 
-        <div className='color-legend'>
-          {colors.map((color, index) => 
-            <div key={index} style={{display: 'flex', alignItems: 'center', gap: '10px', marginTop: '-20px'}}>
+        </div>
+
+        {/* <div className='color-legend'>
+          {colors.map((color, index) =>
+            <div key={index} style={{ display: 'flex', alignItems: 'center', gap: '10px', marginTop: '-20px' }}>
               <div className='color-label' style={{ background: color.color }} />
               <p>{color.mood}</p>
-            </div>  
+            </div>
           )}
-        </div>
+        </div> */}
       </div>
       <Footer />
     </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 export default function Footer() {
   return (
-    <p>
-      Made by <a href = "https://github.com/kostasp31">kostasp31</a> - special attributions to my boredom -_-
+    <p style={{alignContent: 'end'}}>
+      Made by <a href = "https://github.com/kostasp31">kostasp31</a> - Special attributions to my boredom -_-
     </p>
   );
 }
